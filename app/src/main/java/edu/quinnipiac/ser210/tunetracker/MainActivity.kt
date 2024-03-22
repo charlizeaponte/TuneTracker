0341eb82083fd7c6558bd7461f46699fc412fbdf
jubck <julialbock@gmail.com>
2024-03-21 21:45:46 -0400

    some ui updates; updated variable names

--- app/src/main/java/edu/quinnipiac/ser210/tunetracker/MainActivity.kt
+++ app/src/main/java/edu/quinnipiac/ser210/tunetracker/MainActivity.kt
@@ -2,9 +2,7 @@ package edu.quinnipiac.ser210.tunetracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.gson.Gson
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/edu/quinnipiac/ser210/tunetracker/Result.kt
+++ app/src/main/java/edu/quinnipiac/ser210/tunetracker/Result.kt
@@ -1,10 +1,10 @@
 package edu.quinnipiac.ser210.tunetracker
 
 data class Result(
-    val author: String,
+    val artist: String,
     val duration: String,
     val isExplicit: Boolean,
-    val thumbnail: String,
+    val songImage: String,
     val title: String,
     val videoId: String
 )
