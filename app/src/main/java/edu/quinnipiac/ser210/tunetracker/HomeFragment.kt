5ffb908a9f52467a32927d89994476ad8e4e6156
jubck <julialbock@gmail.com>
2024-03-27 19:52:41 -0400

    added toolbear

--- app/src/main/java/edu/quinnipiac/ser210/tunetracker/HomeFragment.kt
+++ app/src/main/java/edu/quinnipiac/ser210/tunetracker/HomeFragment.kt
@@ -1,17 +1,15 @@
 package edu.quinnipiac.ser210.tunetracker
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import edu.quinnipiac.ser210.tunetracker.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class homeFragment : Fragment() {
     lateinit var navController: NavController
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
@@ -19,8 +17,8 @@ class HomeFragment : Fragment() {
     ): View? {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val view = binding.root
+
         binding.homeButton.setOnClickListener {
-            Log.d("homeButton Listener", "Code has been reached")
             view.findNavController()
                 .navigate(R.id.action_homeFragment_to_searchFragment)
         }
@@ -31,4 +29,4 @@ class HomeFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
-    }
+}
--- app/src/main/java/edu/quinnipiac/ser210/tunetracker/MainActivity.kt
+++ app/src/main/java/edu/quinnipiac/ser210/tunetracker/MainActivity.kt
@@ -6,16 +6,59 @@ import android.util.Log
 import androidx.fragment.app.FragmentContainerView
 import androidx.navigation.NavController
 import com.google.android.material.navigation.NavigationView
-
+import android.content.Intent
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.v("MainActivity","Created")
         setContentView(R.layout.activity_main)
 
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_share -> {
+                shareContent()
+                true
+            }
+            R.id.action_settings -> {
+                openSettings()
+                true
+            }
+            R.id.action_help -> {
+                showHelpInfo()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun shareContent() {
+        val shareIntent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, "Check out this great app!")
+            type = "text/plain"
+        }
+        startActivity(Intent.createChooser(shareIntent, null))
+    }
+
+    private fun openSettings() {
+        Toast.makeText(this, "Settings option selected.", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showHelpInfo() {
+        Toast.makeText(this, "App version 1.0. This app uses a RestAPI to look up song lyrics.", Toast.LENGTH_LONG).show()
     }
 }
