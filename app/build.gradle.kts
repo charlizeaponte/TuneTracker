2b1f216a28bf7b2423aca9fcf8b23db41cef79c6
charlizeaponte <charlizeaponte@yahoo.com>
2024-03-27 13:35:49 -0400

    added header

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     testImplementation("junit:junit:4.13.2")
--- app/src/main/java/edu/quinnipiac/ser210/tunetracker/ApiInterface.kt
+++ app/src/main/java/edu/quinnipiac/ser210/tunetracker/ApiInterface.kt
@@ -1,10 +1,13 @@
 package edu.quinnipiac.ser210.tunetracker
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Headers
+
 
 interface ApiInterface {
    //Might need ? after search in the get method
@@ -21,9 +24,14 @@ interface ApiInterface {
 
         fun create() : ApiInterface {
 
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val httpClient = OkHttpClient.Builder()
+            httpClient.addInterceptor(logging) // <-- this is the important line!
             val retrofit = Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl(BASE_URL)
+                .client(httpClient.build())
                 .build()
             return retrofit.create(ApiInterface::class.java)
 
