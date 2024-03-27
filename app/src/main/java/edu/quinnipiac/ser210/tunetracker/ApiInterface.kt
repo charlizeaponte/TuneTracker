6f12a2ca0e9b3d43ffed012f46babcdf5911cdd7
Charlize Aponte <117864983+charlizeaponte@users.noreply.github.com>
2024-03-27 11:00:07 -0400

    Update ApiInterface.kt

--- app/src/main/java/edu/quinnipiac/ser210/tunetracker/ApiInterface.kt
+++ app/src/main/java/edu/quinnipiac/ser210/tunetracker/ApiInterface.kt
@@ -4,10 +4,13 @@ import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Headers
+
 
 interface ApiInterface {
    //Might need ? after search in the get method
     @GET("search")
+     @Headers("X-RapidAPI-Key:832c38ddcfmshee53ce9983b62bfp187fd9jsn18973a142442", "X-RapidAPI-Host:youtube-music-api3.p.rapidapi.com")
     fun getSearch(): Call<ArrayList<searchData?>?>?
     @GET("music/lyrics/plain")
     fun getLyrics(): Call<ArrayList<lyrics?>?>?
