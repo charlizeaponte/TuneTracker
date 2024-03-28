b4ef051334c1bcd79cc8eb2de2223003e33f5f80
jubck <julialbock@gmail.com>
2024-03-27 21:07:46 -0400

    added toolbar and added share and help options. figuring out the color change.

--- app/src/main/java/edu/quinnipiac/ser210/tunetracker/MainActivity.kt
+++ app/src/main/java/edu/quinnipiac/ser210/tunetracker/MainActivity.kt
@@ -2,14 +2,16 @@ package edu.quinnipiac.ser210.tunetracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.FragmentContainerView
 import androidx.navigation.NavController
 import com.google.android.material.navigation.NavigationView
 import android.content.Intent
+import android.graphics.Color
 import android.view.Menu
 import android.view.MenuItem
+import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +22,13 @@ class MainActivity : AppCompatActivity() {
 
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+    }
+
+    private fun applyBackgroundColor() {
+        val sharedPref = getSharedPreferences("AppSettings", MODE_PRIVATE)
+        val color = sharedPref.getInt("BackgroundColor", Color.BLACK)
+        val rootView = findViewById<ViewGroup>(R.id.main_container)
+        rootView.setBackgroundColor(color)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -55,7 +64,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun openSettings() {
-        Toast.makeText(this, "Settings option selected.", Toast.LENGTH_SHORT).show()
+        val colors = arrayOf("Red", "Green", "Blue", "White")
+        val builder = androidx.appcompat.app.AlertDialog.Builder(this)
+        builder.setTitle("Choose a background color")
+        builder.setItems(colors) { _, which ->
+            val chosenColor = when (which) {
+                0 -> Color.RED
+                1 -> Color.GREEN
+                2 -> Color.BLUE
+                3 -> Color.GRAY
+                else -> Color.BLACK
+            }
+            changeBackgroundColor(chosenColor)
+        }
+        builder.show()
+    }
+
+    private fun changeBackgroundColor(color: Int) {
+        // Save the color to SharedPreferences
+        val sharedPref = getSharedPreferences("AppSettings", MODE_PRIVATE)
+        with (sharedPref.edit()) {
+            putInt("BackgroundColor", color)
+            apply()
+        }
     }
 
     private fun showHelpInfo() {
