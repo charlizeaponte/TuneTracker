ad92c772c32119351b00ddb059ecb1467822e620
sarahallerheiligen <sballerheiligen@quinnipiac.edu>
2024-03-30 23:27:49 -0400

    detail fragment working

--- app/src/main/java/edu/quinnipiac/ser210/tunetracker/detailFragment.kt
+++ app/src/main/java/edu/quinnipiac/ser210/tunetracker/detailFragment.kt
@@ -1,9 +1,6 @@
 package edu.quinnipiac.ser210.tunetracker
 
-import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
@@ -14,7 +11,6 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.bumptech.glide.Glide
@@ -23,25 +19,17 @@ import edu.quinnipiac.ser210.tunetracker.api.lyrics.LyricsResult
 import edu.quinnipiac.ser210.tunetracker.api.song.Song
 import edu.quinnipiac.ser210.tunetracker.api.song.SongResult
 import edu.quinnipiac.ser210.tunetracker.databinding.FragmentDetailBinding
-import edu.quinnipiac.ser210.tunetracker.databinding.FragmentHomeBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class detailFragment : Fragment() {
-    private var _binding: FragmentDetailBinding? = null
-    private val binding get() = _binding!!
 
     var song_num = 0
+    var lyrics = "loading lyrics..."
 
+    private lateinit var binding: FragmentDetailBinding
     lateinit var navController: NavController
-
-    // Define the BroadcastReceiver
-    private val themeChangeReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            updateFragmentBackground()
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,10 +41,11 @@ class detailFragment : Fragment() {
             return
         }
         song_num = detailFragmentArgs.fromBundle(bundle).songNum
+        view?.findViewById<TextView>(R.id.lyricText)?.setText(lyrics)
 
     }
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View? {
+        binding = FragmentDetailBinding.inflate(inflater, container, false)
         return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -65,71 +54,42 @@ class detailFragment : Fragment() {
         navController = Navigation.findNavController(view)
 
 
-        view.findViewById<Button>(R.id.returnButton).setOnClickListener {
+        binding.returnButton.setOnClickListener {
             navController.navigate(R.id.action_detailFragment_to_searchFragment)
         }
+
 
 
 //for when the song is passed and passes it to the function
         var song = (savedInstanceState?.getSerializable("Song") as Song?)
 
-        val image: ImageView = view.findViewById(R.id.imageView2)
-        view.findViewById<TextView>(R.id.songNameTextView).text = songs.get(song_num).title
+        val image = binding.imageView2
+        binding.songNameTextView.text = songs[song_num].title
         val artist = songs.get(song_num).author
         val duration = songs.get(song_num).duration
 
-        view.findViewById<TextView>(R.id.artistNameTextView).text = "By " + artist + " " + duration
+        binding.artistNameTextView.text = "$duration By $artist"
+
         Glide.with(requireContext()).load(songs.get(song_num).thumbnail)
             .apply(RequestOptions().centerCrop())
             .into(image)
 
-
-        if(song != null) {
-            onSongReceived(song, view)
-        }
-        updateFragmentBackground()
+        onSongReceived(song , view)
+        binding.lyricText.text = lyrics
 
     }
-    override fun onStart() {
-        super.onStart()
-        context?.registerReceiver(themeChangeReceiver, IntentFilter(MainActivity.THEME_CHANGED_ACTION))
-    }
-    override fun onStop() {
-        super.onStop()
-        // Unregister the receiver
-        context?.unregisterReceiver(themeChangeReceiver)
-    }
 
-    private fun updateFragmentBackground() {
-        // Check if the context and binding are not null before proceeding
-        val context = context ?: return
-        val sharedPrefs = activity?.getSharedPreferences("ThemePrefs", Context.MODE_PRIVATE)
-        val selectedTheme = sharedPrefs?.getString("SelectedTheme", null)
-        val colorResId = when (selectedTheme) {
-            "Green" -> R.color.green
-            "Red" -> R.color.red
-            "Blue" -> R.color.blue
-            "Grey" -> R.color.grey
-            "Black" -> R.color.black
-            else -> R.color.black
-        }
-        val color = ContextCompat.getColor(context, colorResId)
-        _binding?.root?.setBackgroundColor(color)
-    }
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
-    fun onSongReceived(song: Song, view: View) {
+    fun onSongReceived(song: Song?, view: View) {
 
         //sets text from song api to the view
-        view.findViewById<TextView>(R.id.songNameTextView).text = song.title
-        view.findViewById<TextView>(R.id.artistNameTextView).text = song.author
+//        view.findViewById<TextView>(R.id.songNameTextView).text = song.title
+//        view.findViewById<TextView>(R.id.artistNameTextView).text = song.author
 
+        val video_Id = songs.get(song_num).videoId
+        Log.v("API Response", video_Id.toString())
 
 //Api for the lyrics
-        val apiInterface = ApiInterface.create().getLyrics(song.videoId)
+        val apiInterface = ApiInterface.create().getLyrics(video_Id)
         if(apiInterface != null) {
             apiInterface
                 .enqueue(object : Callback<LyricsResult?> {
@@ -138,9 +98,20 @@ class detailFragment : Fragment() {
                         response: Response<LyricsResult?>
                     ) {
                         Log.v("LyricsAPI", "Response Received")
+                        if (response?.body() != null) {
+                            lyrics = (response.body()!! as LyricsResult).description.text
+                            if (lyrics != null) {
+                                Log.v("API Response", "setting lyrics: $lyrics")
+                                binding.lyricText.text = lyrics
+                            } else {
+                                // Handle the case where lyrics is null
+                                Log.e("API Response", "Lyrics is null")
+                                binding.lyricText.text = "no lyrics found"
+
+                            }
+                        }
 
                     }
-
                     override fun onFailure(call: Call<LyricsResult?>, t: Throwable) {
                         if (t != null) {
                             t.message?.let { Log.d("onFailure", it) }
@@ -149,6 +120,7 @@ class detailFragment : Fragment() {
                     }
 
                 })
+
         }
     }
 }
